abbba4bee59ca4182897e84d7247ebf8b22d7d8a
YaoShangTseng <yakushou730@gmail.com>
2017-10-08 13:59:27 +0800

    add rspec for 404 page

--- spec/routing/homepage_spec.rb
+++ spec/routing/homepage_spec.rb
@@ -5,3 +5,11 @@ RSpec.describe "Homepage" do
     expect(get: "/").to route_to(controller: "courses", action: "index")
   end
 end
+
+RSpec.feature "Error Pages" do
+  scenario "user views error page" do
+    visit "/404"
+
+    expect(page).to have_text("The page you were looking for doesn't exist")
+  end
+end
